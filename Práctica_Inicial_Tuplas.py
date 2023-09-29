667e708d6cd9b611d8a7969db3b769bc890a9e59
666borz666 <iloveass3333@gmail.com>
2023-09-29 09:01:42 -0600

    Inicio

---
+++ Práctica_Inicial_Tuplas.py
@@ -0,0 +1,7 @@
+#Elaborado por: Daniel Campos y Junior Monge
+#Fecha de Creación: 29/09/2023 8:55am
+#Ultima Modificación: 29/09/2023
+#Versión: 3.10.6
+#importación de librerias
+
+#creación de funciones
