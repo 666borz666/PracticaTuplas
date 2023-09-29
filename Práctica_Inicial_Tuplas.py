ca989fc1b85f76b9231e7ea1574b14d893802c18
666borz666 <iloveass3333@gmail.com>
2023-09-29 09:25:49 -0600

    Reto 2

--- Práctica_Inicial_Tuplas.py
+++ Práctica_Inicial_Tuplas.py
@@ -5,3 +5,34 @@
 #importación de librerias
 
 #creación de funciones
+#reto 2
+def contarPalabras(tupla1,tupla2):
+    """
+    """
+    contarPalabras=0
+    for a in tupla1:
+        for b in tupla2:
+            for palabras in b.split():
+                if a==palabras:
+                    contarPalabras+=1
+        print("La palabra",a,"se repite",contarPalabras,"veces")
+        contarPalabras=0
+    return
+#reto 3
+
+#reto 4
+
+#reto 5
+
+#programa principal
+#reto 2
+"""
+tupla1=("calor","ayer","el","mañana")
+tupla2=("ayer hizo bastante calor","en el laboratorio hace calor")
+contarPalabras(tupla1,tupla2)
+"""
+#reto 3
+
+#reto 4
+
+#reto 5
